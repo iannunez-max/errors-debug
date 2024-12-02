668b7205e98a110e46a554c73974c56f777a57cc
Ian Nuñez <iannumi24@alumnes.ilerna.com>
2024-12-02 17:02:28 +0100

    solucion 2 y 3

--- matrixdebug/src/matrixdebug/MatrixDebugging.java
+++ matrixdebug/src/matrixdebug/MatrixDebugging.java
@@ -40,7 +40,7 @@ public class MatrixDebugging {
 
         // Paso 3: Intentar acceder a un índice fuera de los límites
         try {
-            System.out.println("\nAccediendo al elemento en [3][0]: " + matrix[3][0]);
+            System.out.println("\nAccediendo al elemento en [2][0]: " + matrix[2][0]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("\nError: " + e.toString());
         }
@@ -49,8 +49,8 @@ public class MatrixDebugging {
         try {
             int sum = 0;
             // Intento de sumar los elementos de la matriz de forma incorrecta
-            for (int i = 0; i <= matrix.length; i++) {  // Error: índice fuera de límites
-                for (int j = 0; j <= matrix[i].length; j++) {  // Error: índice fuera de límites
+            for (int i = 0; i < matrix.length; i++) {  // Error: índice fuera de límites
+                for (int j = 0; j < matrix[i].length; j++) {  // Error: índice fuera de límites
                     sum += matrix[i][j];
                 }
             }
